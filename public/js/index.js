a4fe58e50024ff8d2106a0bc35eaf0492cbbbcc5
haibin <sunhaibin@goyoo.com>
2016-12-08 13:51:03 +0800

    up the 3D js: add the css of perspective

--- public/js/index.js
+++ public/js/index.js
@@ -3,8 +3,8 @@ window.onload = function () {
     var widthOfBox = imgbox.clientWidth;
     var heightOfBox = imgbox.clientHeight;
 
-    var maxX = widthOfBox/65;
-    var maxY = heightOfBox/65;
+    var maxX = widthOfBox/100;
+    var maxY = heightOfBox/100;
     imgbox.onmousemove = function (event) {
         event.stopPropagation();
 
@@ -13,15 +13,15 @@ window.onload = function () {
         var offY = 0.5 - event.offsetY / heightOfBox;
         var rY = offY * maxX;
 
-        imgbox.style.transition = 'transform .2s, box-shadow .05s ease-in-out';
-        imgbox.style.transform = 'rotateX(' + rY + 'deg)' + ' rotateY(' + (-rX-1) + 'deg)' + ' translate3d(0,0,10px)';
-        var shadow = (offX * 20) + 'px ' + (offY * 20) + 'px ' + '20px 2px rgba(0,0,0,0.15)';
-        imgbox.style.boxShadow = shadow;
+        // imgbox.style.transition = 'transform .2s, box-shadow .05s ease-in-out';
+        imgbox.style.transform = 'rotateX(' + rY + 'deg)' + ' rotateY(' + (-rX-0.5) + 'deg)' + ' translate3d(0,0,10px)';
+        // var shadow = (offX * 20) + 'px ' + (offY * 20) + 'px ' + '20px 2px rgba(0,0,0,0.15)';
+        // imgbox.style.boxShadow = shadow;
     }
 
     imgbox.onmouseout = function () {
         imgbox.style.transform = '';
-        imgbox.style.transition = 'all .2s ease-out';
-        imgbox.style.boxShadow = '';
+        // imgbox.style.transition = 'all .2s';
+        // imgbox.style.boxShadow = '';
     }
 }
