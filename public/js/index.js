4e83c8b1b11ecf042d1943fa3a14031d7fe13979
seasidesun <seasidetank@gmail.com>
2017-01-19 23:44:24 +0800

    add the foot auto position

--- public/js/index.js
+++ public/js/index.js
@@ -1,27 +1,29 @@
 window.onload = function () {
-    var imgbox = document.getElementsByClassName('img-box')[0];
-    var widthOfBox = imgbox.clientWidth;
-    var heightOfBox = imgbox.clientHeight;
 
-    var maxX = widthOfBox/100;
-    var maxY = heightOfBox/100;
-    imgbox.onmousemove = function (event) {
-        event.stopPropagation();
+    /* img animation */
+    // var imgbox = document.getElementsByClassName('img-box')[0];
+    // var widthOfBox = imgbox.clientWidth;
+    // var heightOfBox = imgbox.clientHeight;
+    //
+    // var maxX = widthOfBox/100;
+    // var maxY = heightOfBox/100;
+    // imgbox.onmousemove = function (event) {
+    //     event.stopPropagation();
+    //
+    //     var offX = 0.5 - event.offsetX / widthOfBox;
+    //     var rX = offX * maxY;
+    //     var offY = 0.5 - event.offsetY / heightOfBox;
+    //     var rY = offY * maxX;
+    //
+    //     imgbox.style.transform = 'rotateX(' + rY + 'deg)' + ' rotateY(' + (-rX-0.5) + 'deg)' + ' translate3d(0,0,10px)';
+    // }
+    //
+    // imgbox.onmouseout = function () {
+    //     imgbox.style.transform = '';
+    // }
 
-        var offX = 0.5 - event.offsetX / widthOfBox;
-        var rX = offX * maxY;
-        var offY = 0.5 - event.offsetY / heightOfBox;
-        var rY = offY * maxX;
-
-        // imgbox.style.transition = 'transform .2s, box-shadow .05s ease-in-out';
-        imgbox.style.transform = 'rotateX(' + rY + 'deg)' + ' rotateY(' + (-rX-0.5) + 'deg)' + ' translate3d(0,0,10px)';
-        // var shadow = (offX * 20) + 'px ' + (offY * 20) + 'px ' + '20px 2px rgba(0,0,0,0.15)';
-        // imgbox.style.boxShadow = shadow;
-    }
-
-    imgbox.onmouseout = function () {
-        imgbox.style.transform = '';
-        // imgbox.style.transition = 'all .2s';
-        // imgbox.style.boxShadow = '';
+    var foot = document.getElementsByClassName('foot-wrapper')[0];
+    if (foot.offsetTop < window.innerHeight) {
+        foot.style.bottom = '0';
     }
 }
