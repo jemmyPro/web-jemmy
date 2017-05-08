b556e281ede6fda30003410f7ba2a6faf8f4d14f
seasidesun <seasidetank@gmail.com>
2017-05-08 09:29:56 +0800

    foot auto position run before page load

--- public/js/index.js
+++ public/js/index.js
@@ -1,32 +1,35 @@
-window.onload = function () {
-
-    /* img animation */
-    // var imgbox = document.getElementsByClassName('img-box')[0];
-    // var widthOfBox = imgbox.clientWidth;
-    // var heightOfBox = imgbox.clientHeight;
-    //
-    // var maxX = widthOfBox/100;
-    // var maxY = heightOfBox/100;
-    // imgbox.onmousemove = function (event) {
-    //     event.stopPropagation();
-    //
-    //     var offX = 0.5 - event.offsetX / widthOfBox;
-    //     var rX = offX * maxY;
-    //     var offY = 0.5 - event.offsetY / heightOfBox;
-    //     var rY = offY * maxX;
-    //
-    //     imgbox.style.transform = 'rotateX(' + rY + 'deg)' + ' rotateY(' + (-rX-0.5) + 'deg)' + ' translate3d(0,0,10px)';
-    // }
-    //
-    // imgbox.onmouseout = function () {
-    //     imgbox.style.transform = '';
-    // }
-
+(function () {
     // auto the position of the footer
     var foot = document.getElementsByClassName('foot-wrapper')[0];
     if (foot.offsetTop < window.innerHeight) {
         foot.style.bottom = '0';
     }
+})();
+
+
+window.onload = function () {
+
+    /* img animation */
+    /*var imgbox = document.getElementsByClassName('img-box')[0];
+    var widthOfBox = imgbox.clientWidth;
+    var heightOfBox = imgbox.clientHeight;
+
+    var maxX = widthOfBox/100;
+    var maxY = heightOfBox/100;
+    imgbox.onmousemove = function (event) {
+        event.stopPropagation();
+
+        var offX = 0.5 - event.offsetX / widthOfBox;
+        var rX = offX * maxY;
+        var offY = 0.5 - event.offsetY / heightOfBox;
+        var rY = offY * maxX;
+
+        imgbox.style.transform = 'rotateX(' + rY + 'deg)' + ' rotateY(' + (-rX-0.5) + 'deg)' + ' translate3d(0,0,10px)';
+    }
+
+    imgbox.onmouseout = function () {
+        imgbox.style.transform = '';
+    }*/
 
     //show the coop
     var image = new Image();
